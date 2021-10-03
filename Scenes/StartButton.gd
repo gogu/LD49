c001468c62d3a26308b37c2e1f92e2a73e5d3fba
gogu <gogu9909@gmail.com>
2021-10-04 07:45:16 +0800

    Add first click screen.

---
+++ Scenes/StartButton.gd
@@ -0,0 +1,26 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_tree().paused = true
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
+func _on_Button_pressed():
+	get_tree().paused = false
+	print("click")
+	visible = false
+	#get_tree().paused = false
+	pass # Replace with function body.
